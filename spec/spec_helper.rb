55be83a092d4aa2c65d3715fb7ea0c8939e21fec
Cache Hamm <chamm@qualvu.com>
2013-07-23 12:19:05 -0600

    moving participant id out into a constant

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'rspec'
 STUDY_ID = 3571
 ASSIGNMENT_ID = 21410
 RESPONSE_ID = 460688
+PARTICIPANT_ID = 48651
 
 TwentyFourTru::Client.token = 'hcZxQq5bEKndMzbgaqow'
 TwentyFourTru::Client.api_host = 'web1.24tru.dev'
